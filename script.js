e46bcbb251c26049cf3b971e2b91c0416e96b71c
Dhruv Sharma <dsd71213@gmail.com>
2023-10-11 16:57:34 +0530

    Added search function

---
+++ script.js
@@ -0,0 +1,23 @@
+function search() {
+    const nm = document.getElementById("movieName").value;
+    fetch(`https://imdb.p.rapidapi.com/title/find?q=${nm}`, {
+        "method": "GET",
+        "headers": {
+            'X-RapidAPI-Key': '6a268179c9msh2799944107ed12fp1222c4jsn3fcbb1ec4f89',
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const list = data.results
+            list.map((item) => {
+                const name = item.title
+                const poster = item.image.url;
+                const movie = `<li> <img src="${poster}"> <h2>${name}</h2>`
+                document.querySelector('.movies').innerHTML += movie;
+            })
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
