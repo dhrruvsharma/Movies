37873ae8511a7733dbf074d3ae48b3df2983663a
Dhruv Sharma <dsd71213@gmail.com>
2023-10-13 23:54:05 +0530

    Added async await

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-    function search() {
-    const nm = document.getElementById("movieName").value;
-    fetch(`https://imdb.p.rapidapi.com/title/find?q=${nm}`, {
-        "method": "GET",
-        "headers": {
+async function search() {
+    const nm = document.getElementById("movieName").value
+    const url = `https://imdb8.p.rapidapi.com/auto-complete?q=${nm}`;
+    const options = {
+        method: 'GET',
+        headers: {
             'X-RapidAPI-Key': '6a268179c9msh2799944107ed12fp1222c4jsn3fcbb1ec4f89',
-            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
         }
-    })
-        .then(response => response.json())
-        .then(data => {
-            const list = data.results
-            list.map((item) => {
-                const name = item.title
-                const poster = item.image.url;
-                const movie = `<li> <img src="${poster}"> <h2>${name}</h2>`
-                document.querySelector('.movies').innerHTML += movie;
-            })
+
+    };
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const list = await data.d;
+        console.log(list);
+        list.map((item) => {
+            const title = item.l;
+            const poster = item.i.imageUrl;
+            const movie = `<li> <img src="${poster}"> <h2>${title}</h2></li>`
+            document.querySelector(".movies").innerHTML += movie;
         })
-        .catch(err => {
-            console.error(err);
-        });
+    } catch (error) {
+        console.error(error);
+    }
 }
